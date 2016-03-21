32065911f309e50f6db45b73b23ddff03accc724
Gus Dunn <wadunn83@gmail.com>
2016-03-21 16:41:29 -0400

    now able to create and push a new git repo to github

---
+++ {{cookiecutter.repo_name}}/github/push_to_new_remote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl -u '{{cookiecutter.github_username}}' https://api.github.com/user/repos -d "{\"name\":\"{{cookiecutter.repo_name}}\"}"
+
+
+git init
+git add .
+git commit -m "First commit"
+
+# Sets the new remote
+git remote add origin {{cookiecutter.github_url}}
+# Verifies the new remote URL
+git remote -v
+
+# Pushes the changes in your local repository up to the remote repository you specified as the origin
+git push origin master
