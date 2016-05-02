d53e5eb93509423a2a99a0953ff6d6f23c195d4c
Gus Dunn <wadunn83@gmail.com>
2016-05-02 12:25:58 -0400

    main: adds Logs, INPUT_ALL, bump to py35

--- {{cookiecutter.repo_name}}/python/scripts/rule_pyscript.py
+++ {{cookiecutter.repo_name}}/python/scripts/rule_pyscript.py
@@ -3,7 +3,13 @@
 import pandas as pd
 
 
-# Settings
+# functions
+def do_blabla():
+    """Do the blabla."""
+    pass
+
+
+# settings
 setting1 = snakemake.params.setting1
 
 # input
@@ -11,13 +17,13 @@ in_file1 = snakemake.input.in_file1
 in_file2 = snakemake.input.in_file2
 
 
-#output
+# output
 out_file1 = snakemake.output.out_file1
 out_file2 = snakemake.output.out_file2
 
 
 
-
+# business
 
 
 if setting1:
--- {{cookiecutter.repo_name}}/r/scripts/rule_rscript.R
+++ {{cookiecutter.repo_name}}/r/scripts/rule_rscript.R
@@ -3,14 +3,13 @@
 library('coolPkg')
 
 
-# Settings
+# settings
 setting1 = snakemake@params["setting1"]
 
 # input
 in.file1 = snakemake@input["in_file1"]
 in.file2 = snakemake@input["in_file2"]
 
-
-#output
+# output
 out.file1 = snakemake@output["out_file1"]
 out.file2 = snakemake@output["out_file2"]
